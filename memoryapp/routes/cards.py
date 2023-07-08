b456d9d1bad945460e888a7202ae887a5c955535
sztuczka <dsztuczka@gmail.com>
2023-07-08 14:56:36 +0200

    Dodawanie fiszek

--- memoryapp/routes/cards.py
+++ memoryapp/routes/cards.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from memoryapp import app
 from memoryapp.repository import *
 
@@ -7,4 +7,12 @@ from memoryapp.repository import *
 def cards(category_id):
     return jsonify(get_cards(category_id))
 
+@app.route('/categories/<int:category_id>/cards', methods=['POST'])
+def add_card(category_id):
+    r = request.json
+
+    word = r['word']
+    translation = r['translation']
+
+    return jsonify(create_card(category_id,word,translation)), 201
 
